85693ff63c0db2a39d0e822008a449346dbc1ce5
rossfish3r <rfisher@protonify.com>
2023-02-12 18:50:12 -0800

    Updated Code to integers

--- MergeFileData.cpp
+++ MergeFileData.cpp
@@ -1,64 +1,81 @@
-/*Includes*/
 #include <iostream>
 #include <fstream>
 #include <vector>
 
 using namespace std;
 
-int main(){
+int main() {
+    vector<int> data1;
+    vector<int> data2;
+    vector<int> result;
 
-  /*initialize the arrays to store lists*/
-    vector<string> listOne;
-    vector<string> listTwo;
-    string item;
-
-    // Read items from first data file
+    // Open data1.dat for reading
     ifstream file1("data1.dat");
-    if (file1.is_open()) {
-        while (getline(file1, item)) {
-        listOne.push_back(item);
-        }
-        file1.close();
-    } else {
-        cout << "Unable to open file dat1.dat" << endl;
+    if (!file1.is_open()) {
+        cout << "Unable to open file data1.dat" << endl;
         return 1;
     }
 
-     // Read items from second file
-  ifstream file2("data2.dat");
-  if (file2.is_open()) {
-    while (getline(file2, item)) {
-      listTwo.push_back(item);
+    // Read integers from data1.dat and store them in data1 vector
+    int x;
+    while (file1 >> x) {
+        data1.push_back(x);
     }
+
+    // Close data1.dat
+    file1.close();
+
+    // Open data2.dat for reading
+    ifstream file2("data2.dat");
+    if (!file2.is_open()) {
+        cout << "Unable to open file data2.dat" << endl;
+        return 1;
+    }
+
+    // Read integers from data2.dat and store them in data2 vector
+    while (file2 >> x) {
+        data2.push_back(x);
+    }
+
+    // Close data2.dat
     file2.close();
-  } else {
-    cout << "Unable to open file dat2.dat" << endl;
-    return 1;
-  }
 
-    std::cout << "Made it processing the list...\r\n" << std::endl;
-
-    // Merge the two lists into a single list
-  vector<string> items;
-  items.insert(items.end(), listOne.begin(), listOne.end());
-  items.insert(items.end(), listTwo.begin(), listTwo.end());
-
-  std::cout << "Merging lists...\r\n" << std::endl;
-
-  // Write the merged list to a file
-  ofstream file3("output.dat");
-  if (file3.is_open()) {
-    for (const auto& item : items) {
-      file3 << item << endl;
+    // Combine data1 and data2 into result vector
+    int i = 0, j = 0;
+    while (i < data1.size() && j < data2.size()) {
+        result.push_back(data1[i++]);
+        result.push_back(data2[j++]);
     }
-    file3.close();
-  } else {
-    cout << "Unable to open file output.dat" << endl;
-    return 1;
-  }
 
-     std::cout << "***********\r\n**FINI...**\r\n***********" << std::endl;
+    // If one file has more lines than the other, add the remaining elements to result
+    while (i < data1.size()) {
+        result.push_back(data1[i++]);
+    }
+    while (j < data2.size()) {
+        result.push_back(data2[j++]);
+    }
+
+    // Open output.dat for writing
+    ofstream output("output.dat");
+    if (!output.is_open()) {
+        cout << "Unable to open file output.dat for writing" << endl;
+        return 1;
+    }
+
+    // Write result vector to output.dat
+    for (int num : result) {
+        output << num << endl;
+    }
+
+    // Close output.dat
+    output.close();
+
+    // Print result vector to console
+    cout << "Result: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
 
     return 0;
-
 }
